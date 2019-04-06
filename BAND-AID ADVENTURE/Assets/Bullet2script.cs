a7668addd89b4583dd6ecf370a356ec22f94b159
MissionBit <missionbit@93p-MissionBit.local>
2019-04-06 13:34:42 -0700

    hi

---
+++ BAND-AID ADVENTURE/Assets/Bullet2script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Bullet2script : MonoBehaviour
+{
+    Transform tform;
+    Animator anim;
+    Stopwatch stopWatch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.anim = GetComponent<Animator>();
+        stopWatch = new Stopwatch();
+        stopWatch.Start();
+        this.tform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Time", stopWatch.ElapsedMilliseconds/1000);
+        if (stopWatch.ElapsedMilliseconds > 2000)
+        {
+            tform.position = new Vector2(tform.position.x + (float)-1.3, tform.position.y);
+        }
+    }
+}
---
+++ BAND-AID ADVENTURE/Assets/sc.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc : MonoBehaviour
+{
+    Transform Tform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        Tform.position = new Vector2(Tform.position.x + (float) -1.3, Tform.position.y);
+    }
+}
